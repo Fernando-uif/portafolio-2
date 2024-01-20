06c893feb7c8e85929bc1a4ba2bf13b91c9abb22
Fernando Sosa Torres <fsosa@televisa.news>
2024-01-20 17:20:21 -0600

    add the hover on icons

--- src/components/contact/ContactMeIcons.tsx
+++ src/components/contact/ContactMeIcons.tsx
@@ -5,16 +5,25 @@ import contactIconStyle from "@/sass/contact/contactMeIcons.module.scss";
 const ContactMeIcons = () => {
   return (
     <div className={`${contactIconStyle["contactIcons__wrapperName__icons"]}`}>
-      <Link href={"http://wa.me/5523923346"}>
+      <Link
+        href={"http://wa.me/5523923346"}
+        className={`${contactIconStyle["contactIcons--item"]}`}
+      >
         <Icon
           name="phone"
           className={`${contactIconStyle["contactIcons__icon-phone"]}`}
         />
       </Link>
-      <Link href={"https://www.linkedin.com/in/fernando-sosa-fst/"}>
+      <Link
+        href={"https://www.linkedin.com/in/fernando-sosa-fst/"}
+        className={`${contactIconStyle["contactIcons--item"]}`}
+      >
         <Icon name="linkedin" />
       </Link>
-      <Link href={"https://github.com/Fernando-uif"}>
+      <Link
+        href={"https://github.com/Fernando-uif"}
+        className={`${contactIconStyle["contactIcons--item"]}`}
+      >
         <Icon name="github" />
       </Link>
     </div>
